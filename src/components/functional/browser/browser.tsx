6ca0e171f2b4a2552e6c14393944b6b609c2c8a8
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-10-11 17:47:00 +0200

    fix: 🐛 minor routing improvements

--- src/components/functional/browser/browser.tsx
+++ src/components/functional/browser/browser.tsx
@@ -1,22 +1,7 @@
-import { useMemo } from "react"
-import { useRoute } from "../../../routing/provider"
-import { RouteTypes } from "../../../routing/route-types"
-
-export function Browser() {
-    const { route } = useRoute()
-
-    const title = useMemo(() => {
-        if (route instanceof RouteTypes.Browser) {
-            return `Browser – ${route.path || '[root]'}`
-        }
-        throw new Error('Route is not a BrowserRoute')
-    }, [route])
-
+export function Browser(props: { path: string }) {
     return (
-        <box borderColor="cyan" borderStyle="rounded" flexGrow={1} title={` ${title} `} titleAlignment="center" flexDirection="row">
-            <text padding={1}>
-                Browser Component
-            </text>
+        <box borderColor="cyan" borderStyle="rounded" flexGrow={1} title={` ${props.path || '[root]'} `} titleAlignment="center" flexDirection="row">
+            
         </box>
     )
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,19 @@ import { render } from "@opentui/react"
 import { App } from "./app";
 import { loadConfig } from "./config";
 import { initRedis } from "./redis";
+import z from "zod";
+import process from "node:process"
 
-loadConfig();
+try {
+    loadConfig();
+} catch (error) {
+    if (error instanceof z.ZodError) {
+        console.error(`Configuration error!\n ${error.message}`);
+        process.exit(1);
+    }
+    throw error;
+}
+
 initRedis();
 
 render(App());
