b48991a01937a5269323b12f3fb192ae02fd139e
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-10-17 16:45:07 +0200

    feat: ✨ improve key details view

--- src/components/pure/redis-key-type.tsx
+++ src/components/pure/redis-key-type.tsx
@@ -1,3 +1,4 @@
+import Color from "color";
 import { useMemo } from "react";
 
 interface Props {
@@ -9,18 +10,16 @@ const colorhash = (
 	options?: { saturation?: number; lightness: number },
 ) => {
 	const saturation = options?.saturation ?? 50;
-	const lightness = options?.lightness ?? 50;
+	const lightness = options?.lightness ?? 30;
 
 	let hash = 0;
 	for (let i = 0; i < text.length; i++) {
 		hash = text.charCodeAt(i) + ((hash << 5) - hash);
-		hash = hash & hash; // Convert to 32-bit integer
 	}
 
 	// Convert hash to hue (0-360)
 	const hue = Math.abs(hash) % 360;
-
-	return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+	return Color.hsl(hue, saturation, lightness).hex();
 };
 
 export function RedisKeyType(props: Props) {
