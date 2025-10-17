6585ac88dbe9659747cebf2a2ad69c2979a4940a
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-10-17 17:10:27 +0200

    feat: ✨ add global key bindings to quit app

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { useKeyboard } from "@opentui/react";
 import { Browser } from "./components/functional/browser/browser";
 import { EntryDetails } from "./components/functional/entry-detail-view";
 import { Header } from "./components/functional/header";
@@ -11,6 +12,7 @@ export function App() {
 			<box flexDirection="column" height="100%" width="100%">
 				<Header />
 				<Content />
+				<GeneralKeyBindings />
 			</box>
 		</AppProvider>
 	);
@@ -25,4 +27,14 @@ function Content() {
 		return <EntryDetails focussed pathKey={route.pathKey} />;
 	}
 	throw new Error("Unsupported route type");
+}
+
+function GeneralKeyBindings() {
+	useKeyboard((key) => {
+		if ((key.name === "c" && key.ctrl) || key.name === "q") {
+			process.exit(0);
+		}
+	});
+
+	return undefined;
 }
