7ce113e2978df8a2a6bdbd63b21e760e4d5ffe7d
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-10-26 10:25:46 +0100

    fix: 🐛 only allow dev console in development mode

--- src/components/functional/dev-console.tsx
+++ src/components/functional/dev-console.tsx
@@ -4,6 +4,10 @@ export function DevConsole() {
 	const renderer = useRenderer();
 
 	useKeyboard((key) => {
+		// Only if run in dev mode
+		if (process.env.NODE_ENV !== "development") {
+			return;
+		}
 		if (key.ctrl && key.name === "d") {
 			renderer.console.toggle();
 		}
