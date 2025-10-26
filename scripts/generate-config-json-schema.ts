61c099882ccf7d087a3406abcd4776e467ec50fb
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-10-26 08:45:46 +0100

    ci: 👷 fix build

--- scripts/generate-config-json-schema.ts
+++ scripts/generate-config-json-schema.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S npx tsx@4.20.6
+#!/usr/bin/env -S bunx tsx@4.20.6
 
 // This script generates the JSON Schema for the configuration. It is intended to be run in the CI, after which the output can be included in the release artifacts. This way, users can refer to it when writing their configuration files.
 
